64dfbc69a076f344101b228d7d582d1f25a4ba05
Venelin Efremov <ghent360@iqury.us>
2019-12-26 21:41:32 -0800

    Described disabled features.

--- Firmware/pins_mks_genl_2_0.h
+++ Firmware/pins_mks_genl_2_0.h
@@ -12,16 +12,15 @@
 #define TMC2130
 //#define UVLO_SUPPORT
 
-#define AMBIENT_THERMISTOR
+//#define AMBIENT_THERMISTOR
 #define PINDA_THERMISTOR
 
 //#define W25X20CL                 // external 256kB flash
 #define BOOTAPP                  // bootloader support
 
 
-#define SWI2C_SDA      20 //SDA on P3
-#define SWI2C_SCL      21 //SCL on P3
-
+//#define SWI2C_SDA      20 //SDA on P3
+//#define SWI2C_SCL      21 //SCL on P3
 
 
 #define X_TMC2130_CS           41
@@ -71,12 +70,12 @@
 #define HEATER_2_PIN        -1
 #define TEMP_2_PIN          -1
 
-#define TEMP_AMBIENT_PIN     5 //A5
+//#define TEMP_AMBIENT_PIN     5 //A5
 
 #define TEMP_PINDA_PIN       3 //A3
 
-#define VOLT_PWR_PIN         4 //A4
-#define VOLT_BED_PIN         9 //A9
+//#define VOLT_PWR_PIN         4 //A4
+//#define VOLT_BED_PIN         9 //A9
 
 
 #define E0_TMC2130_CS       66
@@ -115,44 +114,44 @@
 
 #define SDCARDDETECT           15
 
-#define TACH_0                 79 // !!! changed from 81 (EINY03)
-#define TACH_1                 80 
+//#define TACH_0                 79 // !!! changed from 81 (EINY03)
+//#define TACH_1                 80 
 
 #define IR_SENSOR_PIN 62 //idler sensor @PK0 (digital pin 62/A8)
 
 // Support for an 8 bit logic analyzer, for example the Saleae.
 // Channels 0-2 are fast, they could generate 2.667Mhz waveform with a software loop.
-#define LOGIC_ANALYZER_CH0		X_MIN_PIN		// PB6
-#define LOGIC_ANALYZER_CH1		Y_MIN_PIN		// PB5
-#define LOGIC_ANALYZER_CH2		53				// PB0 (PROC_nCS)
+//#define LOGIC_ANALYZER_CH0		X_MIN_PIN		// PB6
+//#define LOGIC_ANALYZER_CH1		Y_MIN_PIN		// PB5
+//#define LOGIC_ANALYZER_CH2		53				// PB0 (PROC_nCS)
 // Channels 3-7 are slow, they could generate
 // 0.889Mhz waveform with a software loop and interrupt locking,
 // 1.333MHz waveform without interrupt locking.
-#define LOGIC_ANALYZER_CH3 		73				// PJ3
+//#define LOGIC_ANALYZER_CH3 		73				// PJ3
 // PK0 has no Arduino digital pin assigned, so we set it directly.
-#define WRITE_LOGIC_ANALYZER_CH4(value) if (value) PORTK |= (1 << 0); else PORTK &= ~(1 << 0) // PK0
-#define LOGIC_ANALYZER_CH5		16				// PH0 (RXD2)
-#define LOGIC_ANALYZER_CH6		17				// PH1 (TXD2)
-#define LOGIC_ANALYZER_CH7 		76				// PJ5
+//#define WRITE_LOGIC_ANALYZER_CH4(value) if (value) PORTK |= (1 << 0); else PORTK &= ~(1 << 0) // PK0
+//#define LOGIC_ANALYZER_CH5		16				// PH0 (RXD2)
+//#define LOGIC_ANALYZER_CH6		17				// PH1 (TXD2)
+//#define LOGIC_ANALYZER_CH7 		76				// PJ5
 
-#define LOGIC_ANALYZER_CH0_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH0); WRITE(LOGIC_ANALYZER_CH0, false); } while (0)
-#define LOGIC_ANALYZER_CH1_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH1); WRITE(LOGIC_ANALYZER_CH1, false); } while (0)
-#define LOGIC_ANALYZER_CH2_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH2); WRITE(LOGIC_ANALYZER_CH2, false); } while (0)
-#define LOGIC_ANALYZER_CH3_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH3); WRITE(LOGIC_ANALYZER_CH3, false); } while (0)
-#define LOGIC_ANALYZER_CH4_ENABLE do { DDRK |= 1 << 0; WRITE_LOGIC_ANALYZER_CH4(false); } while (0)
-#define LOGIC_ANALYZER_CH5_ENABLE do { cbi(UCSR2B, TXEN2); cbi(UCSR2B, RXEN2); cbi(UCSR2B, RXCIE2); SET_OUTPUT(LOGIC_ANALYZER_CH5); WRITE(LOGIC_ANALYZER_CH5, false); } while (0)
-#define LOGIC_ANALYZER_CH6_ENABLE do { cbi(UCSR2B, TXEN2); cbi(UCSR2B, RXEN2); cbi(UCSR2B, RXCIE2); SET_OUTPUT(LOGIC_ANALYZER_CH6); WRITE(LOGIC_ANALYZER_CH6, false); } while (0)
-#define LOGIC_ANALYZER_CH7_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH7); WRITE(LOGIC_ANALYZER_CH7, false); } while (0)
+//#define LOGIC_ANALYZER_CH0_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH0); WRITE(LOGIC_ANALYZER_CH0, false); } while (0)
+//#define LOGIC_ANALYZER_CH1_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH1); WRITE(LOGIC_ANALYZER_CH1, false); } while (0)
+//#define LOGIC_ANALYZER_CH2_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH2); WRITE(LOGIC_ANALYZER_CH2, false); } while (0)
+//#define LOGIC_ANALYZER_CH3_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH3); WRITE(LOGIC_ANALYZER_CH3, false); } while (0)
+//#define LOGIC_ANALYZER_CH4_ENABLE do { DDRK |= 1 << 0; WRITE_LOGIC_ANALYZER_CH4(false); } while (0)
+//#define LOGIC_ANALYZER_CH5_ENABLE do { cbi(UCSR2B, TXEN2); cbi(UCSR2B, RXEN2); cbi(UCSR2B, RXCIE2); SET_OUTPUT(LOGIC_ANALYZER_CH5); WRITE(LOGIC_ANALYZER_CH5, false); } while (0)
+//#define LOGIC_ANALYZER_CH6_ENABLE do { cbi(UCSR2B, TXEN2); cbi(UCSR2B, RXEN2); cbi(UCSR2B, RXCIE2); SET_OUTPUT(LOGIC_ANALYZER_CH6); WRITE(LOGIC_ANALYZER_CH6, false); } while (0)
+//#define LOGIC_ANALYZER_CH7_ENABLE do { SET_OUTPUT(LOGIC_ANALYZER_CH7); WRITE(LOGIC_ANALYZER_CH7, false); } while (0)
 
 // Async output on channel 5 of the logical analyzer.
 // Baud rate 2MBit, 9 bits, 1 stop bit.
-#define LOGIC_ANALYZER_SERIAL_TX_ENABLE do { UBRR2H = 0; UBRR2L = 0; UCSR2B = (1 << TXEN2) | (1 << UCSZ02); UCSR2C = 0x06; } while (0)
+//#define LOGIC_ANALYZER_SERIAL_TX_ENABLE do { UBRR2H = 0; UBRR2L = 0; UCSR2B = (1 << TXEN2) | (1 << UCSZ02); UCSR2C = 0x06; } while (0)
 // Non-checked (quicker) variant. Use it if you are sure that the transmit buffer is already empty.
-#define LOGIC_ANALYZER_SERIAL_TX_WRITE_NC(C) do { if (C & 0x100) UCSR2B |= 1; else UCSR2B &= ~1; UDR2 = C; } while (0)
-#define LOGIC_ANALYZER_SERIAL_TX_WRITE(C) do { \
-	/* Wait for empty transmit buffer */ \
-	while (!(UCSR2A & (1<<UDRE2))); \
-	/* Put data into buffer, sends the data */ \
-	LOGIC_ANALYZER_SERIAL_TX_WRITE_NC(C); \
-} while (0)
+//#define LOGIC_ANALYZER_SERIAL_TX_WRITE_NC(C) do { if (C & 0x100) UCSR2B |= 1; else UCSR2B &= ~1; UDR2 = C; } while (0)
+//#define LOGIC_ANALYZER_SERIAL_TX_WRITE(C) do { \
+//	/* Wait for empty transmit buffer */ \
+//	while (!(UCSR2A & (1<<UDRE2))); \
+//	/* Put data into buffer, sends the data */ \
+//	LOGIC_ANALYZER_SERIAL_TX_WRITE_NC(C); \
+//} while (0)
 
